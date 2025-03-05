b78297e406f49f57d7f6ed8b4876cfabf3e410ff
TuNombreDeUsuario <dooswalok@gmail.com>
2025-03-04 20:59:16 -0500

    PLP

--- logout.js
+++ logout.js
@@ -2,6 +2,7 @@
 
 function logout() {
     localStorage.removeItem("authToken"); // Eliminar el token guardado en el almacenamiento local
+    localStorage.removeItem("tokenExpiration"); // Eliminar la fecha de expiración del token
     window.location.href = "index.html"; // Redirigir a la página de inicio
 }
 
